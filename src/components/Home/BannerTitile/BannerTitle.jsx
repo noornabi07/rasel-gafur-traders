0af6e5fd369ad9014a14a4ce237c47b889bc41eb
noornabi07 <noornabiprogram07@gmail.com>
2024-03-16 20:04:49 +0600

    create a marquee bottom to banner

---
+++ src/components/Home/BannerTitile/BannerTitle.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Container from '../../Shared/Container/Container';
+
+const BannerTitle = () => {
+    return (
+        <Container>
+            <div className='mt-14 w-2/3 flex items-start bg-rose-600 py-4 px-8 text-white mx-auto rounded-lg text-2xl font-serif text-center'>
+                <marquee>
+                    <h1>*** এখানে সকল প্রকার ভুট্টা, ধান, গম ও  পাট পাইকারী ও খুচরা মূল্যে ক্রয় বিক্রয় করা হয় *****      এখানে সকল প্রকার ভুট্টা, ধান, গম ও  পাট পাইকারী ও খুচরা মূল্যে ক্রয় বিক্রয় করা হয় *****     এখানে সকল প্রকার ভুট্টা, ধান, গম ও  পাট পাইকারী ও খুচরা মূল্যে ক্রয় বিক্রয় করা হয় *****     এখানে সকল প্রকার ভুট্টা, ধান, গম ও  পাট পাইকারী ও খুচরা মূল্যে ক্রয় বিক্রয় করা হয় *****      এখানে সকল প্রকার ভুট্টা, ধান, গম ও  পাট পাইকারী ও খুচরা মূল্যে ক্রয় বিক্রয় করা হয় ***</h1>
+                </marquee>
+            </div>
+        </Container>
+    );
+};
+
+export default BannerTitle;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import BannerTitle from './BannerTitile/BannerTitle';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <BannerTitle></BannerTitle>
         </div>
     );
 };
